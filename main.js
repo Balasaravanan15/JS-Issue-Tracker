a89b6083700c0afa422165bbe099b48278721c2a
Balasaravanan15 <87692146+Balasaravanan15@users.noreply.github.com>
2021-08-06 13:04:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
-
 function saveIssue(e) {
+    var issueId = chance.guid();
     var issueDesc = document.getElementById('issueDescInput').value;
     var issueSeverity = document.getElementById('issueSeverityInput').value;
     var issueAssignedTo = document.getElementById('issueAssignedToInput').value;
-    var issueId = chance.guid();
     var issueStatus = 'Open';
-
     var issue = {
         id: issueId,
         description: issueDesc,
@@ -15,7 +13,7 @@ function saveIssue(e) {
         status: issueStatus
     }
 
-    if (localStorage.getItem('issues') == null) {
+    if (localStorage.getItem('issues') === null) {
         var issues = [];
         issues.push(issue);
         localStorage.setItem('issues', JSON.stringify(issues));
@@ -24,55 +22,53 @@ function saveIssue(e) {
         issues.push(issue);
         localStorage.setItem('issues', JSON.stringify(issues));
     }
-
     document.getElementById('issueInputForm').reset();
 
     fetchIssues();
-
     e.preventDefault();
 }
 
+
 function setStatusClosed(id) {
     var issues = JSON.parse(localStorage.getItem('issues'));
-
     for (var i = 0; i < issues.length; i++) {
         if (issues[i].id == id) {
-            issues[i].status = 'Closed';
+            issues[i].status = "Closed";
         }
     }
-
     localStorage.setItem('issues', JSON.stringify(issues));
-
     fetchIssues();
 }
-
 function deleteIssue(id) {
     var issues = JSON.parse(localStorage.getItem('issues'));
-
     for (var i = 0; i < issues.length; i++) {
         if (issues[i].id == id) {
             issues.splice(i, 1);
         }
     }
-
     localStorage.setItem('issues', JSON.stringify(issues));
-
     fetchIssues();
 }
 
 function fetchIssues() {
     var issues = JSON.parse(localStorage.getItem('issues'));
-    var issuesListe = document.getElementById('issuesList');
-
+    var issuesList = document.getElementById('issuesList');
     issuesList.innerHTML = '';
-
     for (var i = 0; i < issues.length; i++) {
         var id = issues[i].id;
         var desc = issues[i].description;
         var severity = issues[i].severity;
         var assignedTo = issues[i].assignedTo;
         var status = issues[i].status;
+        issuesList.innerHTML += '<div class="well">' +
+            '<h6>Issue ID: ' + id + '</h6>' +
+            '<p><span class="label label-info">' + status + '</span></p>' +
+            '<h3>' + desc + '</h3>' +
+            '<p><span class="glyphicon glyphicon-time"></span> ' + severity + ' ' +
+            '<span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>' +
+            '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\'' + id + '\')">Close</a> ' +
+            '<a href="#" class="btn btn-danger" onclick="deleteIssue(\'' + id + '\')">Delete</a>' +
+            '</div>';
 
-        issuesList.innerHTML += '<div class="well">' + '<h6>Issue ID: ' + id + '</h6>' + '<p><span class="label label-info">' + status + '</span></p>' +'<h3>' + desc + '</h3>' + '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>' + '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>' + '<a href="#" onclick="setStatusClosed(\'' + id + '\')" class="btn btn-warning">Close</a> ' +'<a href="#" onclick="deleteIssue(\'' + id + '\')" class="btn btn-danger">Delete</a>' +'</div>';
     }
 }
